f96d3d29780df8e22e37ed844a817e3b2dcabaf5
S.LOGESH <130126607+Lokeshsankars@users.noreply.github.com>
2025-02-10 17:59:16 +0530

    Create InvalidLocation.java

---
+++ week2/WarehouseInventoryss/InvalidLocation.java
@@ -0,0 +1,7 @@
+package week2.inventoryManagementSystem;
+
+public class InvalidLocation extends Exception {
+    public InvalidLocationException(String message) {
+        super(message);
+    }
+}
